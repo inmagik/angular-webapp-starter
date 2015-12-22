bbcc28610d30c989cc93156446349fdcf7a4dbf4
Mauro Bianchi <bianchimro@gmail.com>
2015-12-22 23:20:24 +0100

    fixed network config

--- src/js/modules/config/network-config.js
+++ src/js/modules/config/network-config.js
@@ -14,41 +14,6 @@ angular.module('app.networkconfig', ['restangular'])
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
     */
-
-    // Restangular configuration
-    var baseServerUrl = '/api/'//'http://192.168.1.81:8000/api/'//;
-    RestangularProvider.setBaseUrl(baseServerUrl);
-    /* Default config for Restangular */
-    /*
-    RestangularProvider.setDefaultHttpFields({
-        'withCredentials': false
-    });
-    */
-    
-    /* Custom response extractor for Restangular  */
-    /* This one plays well with djangorestframework */
-    /*
-    RestangularProvider.setResponseExtractor(function(response, operation, what, url) {
-    var newResponse;
-    if (operation === "getList") {
-        newResponse = response.results != undefined ? response.results : response;
-        newResponse.metadata = {
-            count : response.count,
-            next : response.next,
-            previous : response.previous,
-            number : response.number,
-        }
-    } else {
-        newResponse = response;
-    }
-        return newResponse;
-    });
-    */
-    
-    /* Restangular requestSuffix, appended to all urls -- plays well with django */
-    /*
-    RestangularProvider.setRequestSuffix('/?');
-    */
     
 })
 
